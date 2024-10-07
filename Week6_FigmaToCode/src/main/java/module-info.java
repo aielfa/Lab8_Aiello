e9f2b8ee7f77a838dee65ad46ee2b905210da65b
Francis Aiello <aielfa@farmingdale.edu>
2024-10-07 17:28:17 -0400

    Fixed Project Push

--- Week6_FigmaToCode/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module edu.farmingdale.week6_figmatocode {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens edu.farmingdale.week6_figmatocode to javafx.fxml;
-    exports edu.farmingdale.week6_figmatocode;
-}
